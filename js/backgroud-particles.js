0eef67a21118bece111ef7d7f520932b8071a3e5
Wellington <wellington.junior@madeiramadeira.com.br>
2024-08-21 23:47:38 -0300

    feat: melhoria no layout

--- js/backgroud-particles.js
+++ js/backgroud-particles.js
@@ -1,22 +1,49 @@
 particlesJS("welcome", {
   particles: {
-    number: { value: 150, density: { enable: true, value_area: 800 } },
-    color: { value: "#ffffff" },
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: "#ffffff"
+    },
     shape: {
       type: "triangle",
-      stroke: { width: 0, color: "#000000" },
-      polygon: { nb_sides: 5 },
-      image: { src: "img/github.svg", width: 100, height: 100 }
+      stroke: {
+        width: 0,
+        color: "#000000"
+      },
+      polygon: {
+        nb_sides: 5
+      },
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100
+      }
     },
     opacity: {
       value: 0.5,
       random: false,
-      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false
+      }
     },
     size: {
       value: 3,
       random: true,
-      anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false
+      }
     },
     line_linked: {
       enable: true,
@@ -33,14 +60,24 @@ particlesJS("welcome", {
       straight: false,
       out_mode: "out",
       bounce: false,
-      attract: { enable: false, rotateX: 600, rotateY: 1200 }
+      attract: { 
+        enable: false, 
+        rotateX: 600, 
+        rotateY: 1200 
+      }
     }
   },
   interactivity: {
     detect_on: "canvas",
     events: {
-      onhover: { enable: true, mode: "repulse" },
-      onclick: { enable: true, mode: "push" },
+      onhover: { 
+        enable: true, 
+        mode: "repulse" 
+      },
+      onclick: { 
+        enable: true, 
+        mode: "push" 
+      },
       resize: true
     },
     modes: {
--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,77 @@
 document.querySelectorAll('.nav__link--toggle').forEach(item => {
-    item.addEventListener('click', function(e) {
-      e.preventDefault(); // Prevenir o comportamento padrão do link
-  
-      const parentItem = this.parentElement;
-  
-      // Fechar qualquer submenu aberto
-      document.querySelectorAll('.nav__item--active').forEach(activeItem => {
-        if (activeItem !== parentItem) {
-          activeItem.classList.remove('nav__item--active');
-          activeItem.querySelector('.nav__submenu').style.maxHeight = 0;
-        }
-      });
-  
-      // Alternar o submenu do item clicado
-      const submenu = parentItem.querySelector('.nav__submenu');
-      if (parentItem.classList.contains('nav__item--active')) {
-        submenu.style.maxHeight = 0;
-        parentItem.classList.remove('nav__item--active');
-      } else {
-        submenu.style.maxHeight = submenu.scrollHeight + 'px';
-        parentItem.classList.add('nav__item--active');
+  item.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const parentItem = this.parentElement;
+
+    document.querySelectorAll('.nav__item--active').forEach(activeItem => {
+      if (activeItem !== parentItem) {
+        activeItem.classList.remove('nav__item--active');
+        activeItem.querySelector('.nav__submenu').style.maxHeight = 0;
       }
     });
+
+    const submenu = parentItem.querySelector('.nav__submenu');
+    if (parentItem.classList.contains('nav__item--active')) {
+      submenu.style.maxHeight = 0;
+      parentItem.classList.remove('nav__item--active');
+    } else {
+      submenu.style.maxHeight = submenu.scrollHeight + 'px';
+      parentItem.classList.add('nav__item--active');
+    }
   });
-  
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const navToggle = document.getElementById('nav-toggle');
+  const sidebar = document.getElementById('sidebar');
+  const wrapper = document.querySelector('.wrapper');
+
+  function handleResize() {
+    if (window.innerWidth <= 1400) {
+      // Ajustes para telas menores
+      sidebar.style.display = 'none'; // Esconde a sidebar por padrão
+      wrapper.style.marginLeft = '0';
+      wrapper.style.width = '100%';
+    } else {
+      // Ajustes para telas maiores
+      sidebar.style.display = 'flex';
+      wrapper.style.marginLeft = '130px';
+      wrapper.style.width = 'calc(100% - 130px)';
+    }
+  }
+
+  // Verifica o tamanho da tela ao carregar a página
+  handleResize();
+
+  // Verifica o tamanho da tela quando a janela é redimensionada
+  window.addEventListener('resize', handleResize);
+
+  navToggle.addEventListener('click', function () {
+    if (window.innerWidth <= 1400) {
+      // Toggle para telas menores
+      sidebar.classList.toggle('sidebar__toggle--show');
+      if (sidebar.classList.contains('sidebar__toggle--show')) {
+        sidebar.style.display = 'flex';
+      } else {
+        sidebar.style.display = 'none';
+      }
+    } else {
+      // Toggle para telas maiores
+      sidebar.classList.toggle('sidebar__toggle--show');
+      if (sidebar.classList.contains('sidebar__toggle--show')) {
+        wrapper.style.marginLeft = '0';
+        wrapper.style.width = '100%';
+      } else {
+        wrapper.style.marginLeft = '130px';
+        wrapper.style.width = 'calc(100% - 130px)';
+      }
+    }
+  });
+});
+
+
+
+
+
