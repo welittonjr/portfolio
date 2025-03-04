e63909e67c69c29beaf8ad369752cdd7bdd5e7bc
Wellington <wellington.junior@madeiramadeira.com.br>
2025-03-04 19:46:08 -0300

    wip: manutenção do css

--- js/scripts.js
+++ js/scripts.js
@@ -24,14 +24,17 @@ $(function () {
 
   const $navToggle = $("#nav-toggle");
   const $sidebar = $("#sidebar");
+  const $header = $("header");
   const $wrapper = $(".wrapper");
 
   function handleResize() {
     if ($(window).width() <= 1400) {
       $sidebar.hide();
+      $header.css({ "margin-left": "0", width: "100%" });
       $wrapper.css({ "margin-left": "0", width: "100%" });
     } else {
       $sidebar.show();
+      $header.css({ "margin-left": "130px", width: "calc(100% - 130px)" });
       $wrapper.css({ "margin-left": "130px", width: "calc(100% - 130px)" });
     }
   }
@@ -43,14 +46,21 @@ $(function () {
   $navToggle.on("click", function () {
     if ($(window).width() <= 1400) {
       $sidebar.toggleClass("sidebar__toggle--show");
-      $sidebar.hasClass("sidebar__toggle--show") ? $sidebar.css("display", "flex") : $sidebar.hide();
+      $sidebar.hasClass("sidebar__toggle--show") ? $sidebar.css("display", "block") : $sidebar.hide();
     } else {
       $sidebar.toggleClass("sidebar__toggle--show");
       if ($sidebar.hasClass("sidebar__toggle--show")) {
+        $header.css({ "margin-left": "0", width: "100%" });
         $wrapper.css({ "margin-left": "0", width: "100%" });
       } else {
+        $header.css({ "margin-left": "130px", width: "calc(100% - 130px)" });
         $wrapper.css({ "margin-left": "130px", width: "calc(100% - 130px)" });
       }
     }
   });
+
+
+  let currentYear = new Date().getFullYear();
+  $(".footer-copyright").html(`Wellington &copy; ${currentYear}. Todos os direitos reservados.`);
+
 });
